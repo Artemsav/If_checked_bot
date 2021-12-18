d0b81949cea90d8182a290b2f7a5462ba7e79438
Artemsav <31589472+Artemsav@users.noreply.github.com>
2021-12-18 16:51:56 +0300

    Delete unnecessary libs,prints, fix handle_status

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import os
 import json
 import logging
-import pprint
 from pathlib import Path
 
 import requests
-import telegram
 from dotenv import load_dotenv
 from environs import Env
 from telegram import Bot
-from telegram.ext import Filters, Updater, dispatcher
-from telegram.ext import callbackcontext
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.callbackqueryhandler import CallbackQueryHandler
 
 env_path = Path('.', '.env')
 load_dotenv(dotenv_path=env_path,override=True)
@@ -26,7 +19,6 @@ payload = {'timestamp_to_request': ''}
 token=os.getenv('TOKEN_TELEGRAM')
 user_id=os.getenv('USER_ID')
 bot = Bot(token=token)
-#dispatcher = bot.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
@@ -34,22 +26,19 @@ def parse_status(url, headers, payload):
     r = requests.get(url, headers=headers, params=payload)
     review_status = r.json()
     timestamp_to_request = review_status.get('timestamp_to_request')
-    pprint.pprint(review_status, indent = 4, depth = 4)
     payload = {'timestamp_to_request':timestamp_to_request}
     return review_status
 
 def handle_status(status):
-    title = status.get('lesson_title')
-    print(status.get('is_negative'))
     for item in status.get('new_attempts'):
-        print(item)
+        title =  item.get('lesson_title')
+        link_on_task = item.get('lesson_url')
         if item.get('is_negative')==True:
-            status_of_work = f'У вас проверили работу {title} \n К сожалению, в работе нашлись ошибки'
+            status_of_work = f'У вас проверили работу "{title}" \n К сожалению, в работе нашлись ошибки. Ссылка на задачу {link_on_task}'
         elif item.get('is_negative')==False:
-            status_of_work = f'У вас проверили работу {title} \n Преподавателю все понравилось, можно приступать к следующему уроку!'
+            status_of_work = f'У вас проверили работу "{title}" \n Преподавателю все понравилось, можно приступать к следующему уроку!'
         else:
             status_of_work = 'Status is incorrect, please try again'
-        print(status_of_work)
         return status_of_work
 
 def start (update, context):
@@ -60,18 +49,11 @@ def start (update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text = f'Hello {chat_user_client}!')
 
-'''def status (update, context):
-    callbackcontext
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text = f'Teacher has reviewed your {main(url, headers, payload)} project!')'''
 
 if __name__ == '__main__':
-    #start_handler = CommandHandler('start', start)
-    #dispatcher.add_handler(start_handler)
-    #bot.start_polling()
     while True:
         try:
-            handle_status(parse_status(url, headers, payload))
+            parse_status(url, headers, payload)
             bot.send_message(chat_id = user_id, text = handle_status(parse_status(url, headers, payload)))
 
         except ConnectionError:
