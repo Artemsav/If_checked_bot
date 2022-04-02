a8d83c55df21a86ddd498f65ea10ee8c132cff38
artemsav <studentbedn@gmail.com>
2022-04-02 08:28:33 +0300

    derrived class from main function, renaim vars in main and handle_status func

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ from dotenv import load_dotenv
 from requests.models import ReadTimeoutError
 from telegram import Bot
 
+logger = logging.getLogger('Bot logging')
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
 
 def main():
     load_dotenv()
@@ -19,33 +33,20 @@ def main():
     user_id = os.getenv('USER_ID')
     bot = Bot(token=token)
     logging_bot = Bot(token=logging_token)
-
-
-    class TelegramLogsHandler(logging.Handler):
-
-        def __init__(self, tg_bot, chat_id):
-            super().__init__()
-            self.chat_id = chat_id
-            self.tg_bot = tg_bot
-
-        def emit(self, record):
-            log_entry = self.format(record)
-            self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
-
-
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger('Bot logging')
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger.setLevel(logging.DEBUG)
     logger.addHandler(TelegramLogsHandler(tg_bot=logging_bot, chat_id=user_id))
     logger.info('Бот запущен')
     while True:
         try:
-            status = get_status(url, headers, payload)
-            if status.get('status')=='found':
-                bot.send_message(chat_id=user_id, text=handle_status(status))
-                payload = {'timestamp': status.get('last_attempt_timestamp')}
+            r = requests.get(url, headers=headers, params=payload, timeout=60)
+            api_respond = r.json()
+            if api_respond.get('status') == 'found':
+                bot.send_message(chat_id=user_id, text=handle_status(api_respond))
+                payload = {'timestamp': api_respond.get('last_attempt_timestamp')}
             else:
-                payload = {'timestamp': status.get('timestamp_to_request')}
+                payload = {'timestamp': api_respond.get('timestamp_to_request')}
         except ConnectionError as exc:
             logger.error(exc)
             time.sleep(60)
@@ -53,15 +54,9 @@ def main():
             pass
 
 
-def get_status(url, headers, payload):
-    r = requests.get(url, headers=headers, params=payload, timeout=60)
-    api_respond = r.json()
-    return api_respond
-
-
-def handle_status(status):
+def handle_status(message):
     works_status = []
-    for attempt in status.get('new_attempts'):
+    for attempt in message.get('new_attempts'):
         title = attempt.get('lesson_title')
         task_link = attempt.get('lesson_url')
         if attempt.get('is_negative'):
